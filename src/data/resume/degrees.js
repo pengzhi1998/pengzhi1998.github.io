7e23b56c3ab54f5d319b9ba0b4f83ff7bd8e1f05
Penzhi <tyypz2590477658@gmail.com>
2022-10-18 19:53:38 +0200

    sidebar

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,15 +1,27 @@
 const degrees = [
   {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering',
-    link: 'https://stanford.edu',
-    year: 2016,
+    school: 'TU Delft',
+    degree: 'M.S. Computer Science in AIT',
+    link: 'https://www.tudelft.nl/onderwijs/opleidingen/masters/cs/msc-computer-science',
+    year: 2024,
   },
   {
-    school: 'University at Buffalo',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
-    link: 'https://buffalo.edu',
-    year: 2012,
+    school: 'University of California, San Diego (UCSD)',
+    degree: 'M.S. Computer Science, CS75 (Interrupted due to US-visa ban)',
+    link: 'https://cse.ucsd.edu/',
+    year: 2021,
+  },
+  {
+    school: 'University of California, Santa Barbara (UCSB)',
+    degree: 'Exchange Program in Computer Science (Spring Semester)',
+    link: 'https://www.cs.ucsb.edu/',
+    year: 2019,
+  },
+  {
+    school: 'University of Electronic Science and Technology of China (UESTC)',
+    degree: 'B.E. Computer Science and Technology, Yingcai Honors College',
+    link: 'https://en.uestc.edu.cn/',
+    year: 2020,
   },
 ];
 
