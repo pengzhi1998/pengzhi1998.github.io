cd23ca40924d9d254663636659fcb5a9fe486402
pengzhi1998 <tyypz2590477658@gmail.com>
2022-12-04 03:20:25 +0100

    projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,47 +1,51 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Nearest Dollar',
-    subtitle: '2015 BVP Hackathon',
-    image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
+    title: '3D Print Catamaran',
+    subtitle: '3D Print Catamaran',
+    link: '/files/Xcavenger.mp4',
+    image: '/images/projects/model.jpg',
+    date: '2018-10-01',
     desc:
-      'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+      'We designed a small catamaran which could capture the floating trash and 3D printed it. By differentiating the speed of two motors, the direction of the surface robot could be changed easily.',
   },
   {
-    title: 'Harvest',
-    subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
-    link: 'https://devpost.com/software/harvest',
-    image: '/images/projects/harvest.jpg',
-    date: '2015-09-20',
-    desc:
-      'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
-      + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
-      + 'and nutrient deficiencies at an affordable price for the developing world.',
-  },
-  {
-    title: 'Space Potato',
+    title: 'Simple Compiler Implementation',
     subtitle: 'A kickstarter funded potato powered weather balloon.',
-    link: 'http://www.spacepotato.org',
-    image: '/images/projects/spacepotato.jpg',
+    image: '/images/projects/compiler.jpg',
     date: '2015-06-28',
     desc:
-      'Launched a potato battery powered weather balloon with two cameras '
-      + 'and gps transponder. Resulting photos were published in a coffee table book. '
-      + 'You can email me for a copy.',
+      'Course project of Translation of Programming Languages (CS160). The 6 projects included Scanner, Parser, Abstract Syntax Tree (AST), Type Checking and Code Generation.\n'
+      + 'Completed the project which comprised major components of an intact compiler for C++. All test cases parsed successfully with no errors and outputted valid X86 assembly code.\n'
+      + 'Received a grade of A+ for the course.',
   },
   {
-    title: 'Cat Detector',
-    subtitle: 'A convolutional neural network to classify cats! (and dogs)',
-    image: '/images/projects/catdetector.jpg',
-    date: '2015-05-15',
+    title: 'Indoor Localization',
+    subtitle: 'Indoor Localization',
+    link: 'https://github.com/pengzhi1998/Localization-VizDoom',
+    image: '/images/projects/localization_example.gif',
+    date: '2020-05-30',
     desc:
-      'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
-      + 'Over 60,000 cats were classified before server bills made the project too expensive '
-      + 'to continue hosting.',
+      'Implementation for Active Neural Localization. ',
+  },
+  {
+    title: 'Image Filter',
+    subtitle: 'Image Filter',
+    link: 'https://github.com/pengzhi1998/Image-Processing-Experiment',
+    image: '/images/projects/image_filter.jpg',
+    date: '2018-01-01',
+    desc:
+      'Using filters to process the images. ',
+  },
+  {
+    title: 'Simulation on Dissipation of Computer Framecase',
+    subtitle: 'Simulation on Dissipation of Computer Framecase',
+    link: '/files/Computer_Case_Dissipation.pdf',
+    image: '/images/projects/flotherm.jpg',
+    date: '2018-01-20',
+    desc:
+      'Built a computer mainframe case in Flotherm. Adjusted the structure of the whole case and the parameters of the accessories according to thermodynamic theory and simulation results; kept the temperature of three monitored points below 90 ℃.'
+      + ' Proposed an original design of the thermal conductivity structure; the dissipation efficiency was improved.',
   },
 ];
 
