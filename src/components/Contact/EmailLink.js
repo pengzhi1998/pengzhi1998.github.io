f5417fec5f2cb5c8921cd20e7964f1d87c1b7374
pengzhi1998 <tyypz2590477658@gmail.com>
2022-12-04 04:32:18 +0100

    head

--- src/components/Contact/EmailLink.js
+++ src/components/Contact/EmailLink.js
@@ -84,7 +84,7 @@ const EmailLink = ({ loopMessage }) => {
     >
       <a href={validateText(message) ? `mailto:${message}@mldangelo.com` : ''}>
         <span>{message}</span>
-        <span>@mldangelo.com</span>
+        <span>@pengzhi1998.com</span>
       </a>
     </div>
   );
