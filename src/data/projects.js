2983a09cfd1298fbd47f94981504befffb67154c
pengzhi1998 <tyypz2590477658@gmail.com>
2022-12-04 04:12:48 +0100

    projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,33 @@
 // TODO Add a couple lines about each project
 const data = [
   {
+    title: 'Landmark-based Active Mapping',
+    subtitle: 'Landmark-based Active Mapping',
+    link: 'https://arxiv.org/pdf/2209.12427.pdf',
+    image: '/images/projects/lm.jpg',
+    date: '2022-11-30',
+    desc:
+      'Landmark-based active mapping and exploration. Papers have been submitted.',
+  },
+  {
+    title: 'Underwater Navigation',
+    subtitle: 'Underwater Navigation',
+    link: 'https://github.com/pengzhi1998/Robotics_Navigation',
+    image: '/images/projects/AUV.jpg',
+    date: '2022-07-15',
+    desc:
+      'Underwater vehicle End-to-end navigation. Paper published in ISRR 2022.',
+  },
+  {
+    title: 'Quadruped Robot Locomotion',
+    subtitle: 'Quadruped Robot Locomotion',
+    // link: '/files/Xcavenger.mp4',
+    image: '/images/projects/tencent.jpg',
+    date: '2021-08-14',
+    desc:
+      'Quadruped Robot Locomotion with DRL.',
+  },
+  {
     title: '3D Print Catamaran',
     subtitle: '3D Print Catamaran',
     link: '/files/Xcavenger.mp4',
@@ -13,7 +40,7 @@ const data = [
     title: 'Simple Compiler Implementation',
     subtitle: 'A kickstarter funded potato powered weather balloon.',
     image: '/images/projects/compiler.jpg',
-    date: '2015-06-28',
+    date: '2019-06-15',
     desc:
       'Course project of Translation of Programming Languages (CS160). The 6 projects included Scanner, Parser, Abstract Syntax Tree (AST), Type Checking and Code Generation.\n'
       + 'Completed the project which comprised major components of an intact compiler for C++. All test cases parsed successfully with no errors and outputted valid X86 assembly code.\n'
@@ -29,6 +56,16 @@ const data = [
       'Implementation for Active Neural Localization. ',
   },
   {
+    title: 'PIPELINED CPU IMPLEMENTED ON FPGA',
+    subtitle: 'PIPELINED CPU IMPLEMENTED ON FPGA',
+    link: 'https://github.com/pengzhi1998/Deep-Pipelined-CPU-on-FPGA',
+    image: '/images/projects/x86.jpg',
+    date: '2018-07-05',
+    desc:
+      'CPU designed and implemented by Zhongyao Cao, Sufang Yang as well as Pengzhi Yang. All the parts were written in verilog.'
+      + 'This is the curriculum design for our computer achitecture course. We built our own 5 staged pipelined CPU and added deep pipeline in ALU to make its efficiency much higher. And we ran the FFT algorithm on it and implemented it on FPGA.',
+  },
+  {
     title: 'Image Filter',
     subtitle: 'Image Filter',
     link: 'https://github.com/pengzhi1998/Image-Processing-Experiment',
@@ -36,6 +73,15 @@ const data = [
     date: '2018-01-01',
     desc:
       'Using filters to process the images. ',
+  },
+  {
+    title: 'Research on Magnetic Field Energy',
+    subtitle: 'Research on Magnetic Field Energy',
+    link: 'http://wltb.cnjournals.com/ch/reader/create_pdf.aspx?file_no=20181649&flag=1&journal_id=wltb&year_id=2019',
+    image: '/images/projects/magnetic.jpg',
+    date: '2019-01-01',
+    desc:
+      'Magnetic Field Energy of Two Parallel Current-carrying Straight Wires. ',
   },
   {
     title: 'Simulation on Dissipation of Computer Framecase',
